cec91938b41dc2773843e943bb06bc56d5b13d5f
Vince Swu <vinceswu88@gmail.com>
2025-09-21 20:57:51 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from dotenv import load_dotenv
+import logging
+
+# -------------------------------
+# Logging Configuration
+# -------------------------------
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s - %(message)s',
+                    force=True)
 
 class Core:
     def __init__(self) -> None:
-        pass
+        self.logger = logging.getLogger(self.__class__.__name__)
 
 if __name__ == "__main__":
     load_dotenv()
